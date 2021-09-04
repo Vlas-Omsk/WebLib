acd52ff1d6ba35041086a4c52963733d5ba85696
Vlas-Omsk <62666113+Vlas-Omsk@users.noreply.github.com>
2021-09-04 10:57:03 +0600

    #0 Upload

---
+++ Response.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using PinkJson;
+
+namespace WebLib
+{
+    public class Response
+    {
+        public string Content { get; internal set; }
+        public WebHeaderCollection Headers { get; internal set; }
+        public bool IsHaveErrors { get; internal set; }
+        public HttpStatusCode StatusCode { get; internal set; }
+
+        internal void SetHeaders(WebHeaderCollection collection)
+        {
+            Headers = collection;
+            //var headers = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            //for (int i = 0; i < collection.Count; ++i)
+            //{
+            //    string key = collection.GetKey(i);
+            //    foreach (string value in collection.GetValues(i))
+            //        headers.Add(key, value);
+            //}
+            //Headers = headers;
+        }
+
+        public Json GetJson()
+        {
+            return new Json(Content);
+        }
+    }
+}
---
+++ Web.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace WebLib
+{
+    public class Web
+    {
+        public Response SendRequest(string url, RequestOptions options)
+        {
+            options.Url = url;
+            return SendRequest(options);
+        }
+
+        public Response SendRequest(RequestOptions options)
+        {
+            return SendRequest(options.GetHttpWebRequest());
+        }
+
+        public Response SendRequest(HttpWebRequest request)
+        {
+            Response response = new Response();
+            HttpWebResponse webresponse;
+            try
+            {
+                webresponse = (HttpWebResponse)request.GetResponse();
+            } catch (WebException ex)
+            {
+                webresponse = (HttpWebResponse)ex.Response;
+            }
+
+            using (Stream s = webresponse.GetResponseStream())
+            {
+                response.StatusCode = webresponse.StatusCode;
+                response.SetHeaders(webresponse.Headers);
+                using (StreamReader sr = new StreamReader(s))
+                    response.Content = sr.ReadToEnd();
+            }
+
+            return response;
+        }
+    }
+}
